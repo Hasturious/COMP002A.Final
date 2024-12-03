8eb63baac059e30c9adeb94fedc63adf9f84f5e8
Jean Bryant Figueroa <jbster559@gmail.com>
2024-12-03 15:48:29 -0800

    Initial Commit - Added const for selectors and a new button with event listener

---
+++ script.js
@@ -0,0 +1,10 @@
+// Selectors
+const cells = document.querySelectorAll(".gamesquare");
+const trackTurns = document.getElementById("turn");
+const playAgain = document.getElementById("button-playagain");
+const resetScore = document.getElementById("buttonresetscore")
+const scoreboardX = document.getElementById("scoreboardx");
+const scoreboardO = document.getElementById("scoreboardo");
+
+playAgain.addEventListener("click");
+resetScore.addEventListener("click")
