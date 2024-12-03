bbb0d444cee145f612a9416226f72c42fb04bef0
Jean Bryant Figueroa <jbster559@gmail.com>
2024-12-03 15:54:39 -0800

    Added winPattern array

--- script.js
+++ script.js
@@ -6,5 +6,9 @@ const resetScore = document.getElementById("buttonresetscore")
 const scoreboardX = document.getElementById("scoreboardx");
 const scoreboardO = document.getElementById("scoreboardo");
 
+const checkWinner = () => {
+    const winPatterns = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ]; //This is a list of all 8 patterns a player can win with
+}
+
 playAgain.addEventListener("click");
-resetScore.addEventListener("click")
+resetScore.addEventListener("click");
