6ebd2caa538cfea8d65dc96e80159f46841d3401
Jean Bryant Figueroa <jbster559@gmail.com>
2024-12-03 16:26:20 -0800

    Updated selectors to match HTML versions and added Varibles

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 // Selectors
-const cells = document.querySelectorAll(".gamesquare");
+const cells = document.querySelectorAll(".game-square");
 const trackTurns = document.getElementById("turn");
 const playAgainButton = document.getElementById("button-playagain");
-const resetScoreButton = document.getElementById("buttonresetscore")
-const scoreboardX = document.getElementById("scoreboardx");
-const scoreboardO = document.getElementById("scoreboardo");
+const resetScoreButton = document.getElementById("button-reset-score")
+const scoreboardX = document.getElementById("scoreboard-x");
+const scoreboardO = document.getElementById("scoreboard-o");
 
+// Varibles
 let isXTurn = true;
 let boardState = Array(9).fill(null);
 let scoreX = sessionStorage.getItem('scoreX') ? parseInt(sessionStorage.getItem('scoreX')) : 0;
 let scoreO = sessionStorage.getItem('scoreO') ? parseInt(sessionStorage.getItem('scoreO')) : 0
 
+// Set Scores
+scoreboardX.textContent = scoreX;
+scoreboardO.textContent = scoreO;
+
 const checkWinner = () => {
     const winPatterns = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ]; //This is a list of all 8 patterns a player can win with
+}
+
+function resetScore() { //defines the resetScore function
+    sessionStorage.clear() //clears stored data
 }
 
 playAgainButton.addEventListener("click");
