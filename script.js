051c3e0f069c2ff9fabca9b7fc3a96674e34cd67
Jean Bryant Figueroa <jbster559@gmail.com>
2024-12-03 17:59:45 -0800

    Added winner detection

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Selectors
+//Selectors
 const cells = document.querySelectorAll(".game-square");
 const trackTurns = document.getElementById("turn");
 const playAgainButton = document.getElementById("button-play-again");
@@ -6,23 +6,43 @@ const resetScoreButton = document.getElementById("button-reset-score")
 const scoreboardX = document.getElementById("scoreboard-x");
 const scoreboardO = document.getElementById("scoreboard-o");
 
-// Varibles
+//Varibles
 let XTurn = true;
 let boardState = Array(9).fill(null);
 let scoreX = sessionStorage.getItem('scoreX') ? parseInt(sessionStorage.getItem('scoreX')) : 0;
 let scoreO = sessionStorage.getItem('scoreO') ? parseInt(sessionStorage.getItem('scoreO')) : 0
 
-// Set Scores
+//Set Scores
 scoreboardX.textContent = scoreX;
 scoreboardO.textContent = scoreO;
 
+/*
+0 | 1 | 2
+---------
+3 | 4 | 5
+---------
+6 | 7 | 8
+
+a diagram that points to each position of the tictactoe board
+*/
 const checkWinner = () => {
-    const winPatterns = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ]; //This is a list of all 8 patterns a player can win with
+    const winPatterns = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], //row
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], //column 
+    [0, 4, 8], [2, 4, 6] ]; //diagonal
+    //This is a list of all 8 patterns a player can win with
+    for (const pattern of winPatterns) {
+        const [a, b, c] = pattern;
+    
+        if (boardState[a] && boardState[a] === boardState[b] && boardState[a] === boardState[c])
+        {
+            return boardState[a]; // Return 'X' or 'O' as the winner
+        }
+    }
+    return boardState.includes(null) ? null : 'Tie'; //If no winner and no empty cells, return 'Tie'   
 }
 
-function resetScore() { //defines the resetScore function
-    sessionStorage.clear() //clears stored data
-}
+
 
 function resetGame() {
     boardState.fill(null);
